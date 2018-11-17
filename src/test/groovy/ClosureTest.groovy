deb4bda7a09cab3c16f0a388ae2b883499fedf9f
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-17 16:15:54 +0100

    ClosureTest.groovy transforming method to closure

--- src/test/groovy/ClosureTest.groovy
+++ src/test/groovy/ClosureTest.groovy
@@ -40,4 +40,49 @@ class ClosureTest  extends Specification {
         then:
         counter == 1
     }
+
+    def "String closure memoization"() {
+        given:
+        def counter = 0
+
+        def closure = {
+            counter++
+            it
+        }.memoize()
+
+        when:
+        closure("a")
+        closure("a")
+        closure("a")
+        closure("b")
+
+        then:
+        counter == 2
+    }
+
+    def "closure from method memoization"() {
+        given:
+        def memoizedExtractId = Functions.&extractId.memoize()
+        
+        def person1 = new Person(id: 1, counter: 0)
+        def person1Copy = new Person(id: 1, counter: 0)
+        def person2 = new Person(id: 2, counter: 0)
+        def person3 = new Person(id: 3, counter: 0)
+
+        when:
+        memoizedExtractId(person1)
+        memoizedExtractId(person1)
+        memoizedExtractId(person1Copy)
+        memoizedExtractId(person1)
+
+        and:
+        memoizedExtractId(person2)
+        memoizedExtractId(person2)
+
+        then:
+        person1.counter == 1
+        person1Copy.counter == 0
+        person2.counter == 1
+        person3.counter == 0
+    }
 }
